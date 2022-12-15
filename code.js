2afc74410b85a8d0906d7f638c850a56484b8796
김한울 <gimhan-ul@gimhan-ul-ui-MacBookAir.local>
2022-12-15 16:32:02 +0900

    edit

--- code.js
+++ code.js
@@ -1,28 +1,31 @@
 const headline = document.querySelector("h1");
 const container = document.querySelector(".qcontainer").children;
-const leftBtn = document.querySelector("#이전");
-const rightBtn = document.querySelector("#다음");
-let index = 0;
-let flex = 0;
+const inputBtns = document.querySelectorAll(".inputBtn");
+
+let index = 0;  
 let str = 0;
-function clickRightBtn() {
+let flex = 0;
+function clickInputBtn() {
     if (index + 1 < container.length) {
         index += 1;
         container[index - 1].classList.add("div");
         container[index].classList.remove("div");
         container[index].value 
+    }else{
+        goResult();
     }
 }
-function clickLeftBtn() {
-    if (index - 1 > -1) {
-        index -= 1;
-        container[index + 1].classList.add("div");
-        container[index].classList.remove("div");
-    }
+
+function goResult() {
+    
 }
+
 function goHome() {
     location.href = "index.html";
 }
-rightBtn.addEventListener("click", clickRightBtn);
-leftBtn.addEventListener("click", clickLeftBtn);
+
+[].forEach.call(inputBtns ,function(col){ 
+	col.addEventListener("click", clickInputBtn); 
+}); 
+
 headline.addEventListener("click", goHome);
